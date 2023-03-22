644ec7ebf0b3c3d152eb402890130022ab7b41fb
augusto <augusdpmenezes@gmail.com>
2023-03-22 16:30:00 -0300

    save

--- proj-clima/src/App.jsx
+++ proj-clima/src/App.jsx
@@ -41,10 +41,10 @@ function App() {
   return (
     <>
       <div className=' bg-opacity-90 bg-cover bg-center flex items-center justify-center flex-col bg-slate-100 h-screen w-full'>
-        <div className='h-2/3  w-96 rounded-xl flex-col flex bg-violet-600  shadow-black shadow-2xl' >
+        <div className='h-2/3 w-3/4 sm:w-3/4 md:w-2/4 lg:w-1/4 rounded-xl flex-col flex bg-sky-400  shadow-sky-900 shadow-md' >
           <form onSubmit={handleSubmit} className='w-full h-1/6 flex justify-center items-center'>
             <div className='flex flex-row h-10 bg-transparent self-start mt-10 w-11/12' >
-              <input value={city} onChange={handleChange} placeholder='Digite a cidade que deseja buscar: ' type="text" className='capitalize text-center text-white w-10/12 bg-transparent rounded-l border border-slate-400 hover:cursor-pointer ' />
+              <input value={city} onChange={handleChange} placeholder='Digite a cidade que deseja buscar: ' type="text" className='capitalize text-center text-white w-10/12 bg-transparent rounded-l border border-slate-300 hover:cursor-pointer ' />
               <div className='ml-auto w-2/12 bg-white rounded-r flex items-center justify-center'>
                 <button type="submit" className='w-full h-full relative hover:bg-slate-200 rounded-r'>
                   <IoIosSearch className='absolute inset-0 m-auto w-3/5 h-3/5' />
